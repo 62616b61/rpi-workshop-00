1088919d7620caffccd3ed02becf72acb2d06dc7
Cry <alexanderkatsero@gmail.com>
2017-09-11 23:50:23 +0300

    async fix

--- app.js
+++ app.js
@@ -8,12 +8,8 @@ gpio.setup(RED_GPIO_PIN, gpio.DIR_HIGH)
 gpio.setup(GREEN_GPIO_PIN, gpio.DIR_HIGH)
 gpio.setup(BLUE_GPIO_PIN, gpio.DIR_HIGH)
 
-gpio.write(RED_GPIO_PIN, false, (err) => {
-  if (err) throw err
-})
-gpio.write(GREEN_GPIO_PIN, false, (err) => {
-  if (err) throw err
-})
-gpio.write(BLUE_GPIO_PIN, false, (err) => {
-  if (err) throw err
-})
+setTimeout(() => {
+  gpio.write(RED_GPIO_PIN, false)
+  gpio.write(GREEN_GPIO_PIN, false)
+  gpio.write(BLUE_GPIO_PIN, false)
+}, 1000)
