bdfc4adb1bbce7be9abb7f71d02853e3b270e8a5
Cry <alexanderkatsero@gmail.com>
2017-09-11 23:33:05 +0300

    set initial state of gpio pins to HIGH

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var RED_GPIO_PIN = 11
 var GREEN_GPIO_PIN = 13
 var BLUE_GPIO_PIN = 15
 
-gpio.setup(RED_GPIO_PIN, gpio.DIR_OUT, gpio.EDGE_NONE, pinSet(RED_GPIO_PIN));
-gpio.setup(GREEN_GPIO_PIN, gpio.DIR_OUT, gpio.EDGE_NONE, pinSet(GREEN_GPIO_PIN));
-gpio.setup(BLUE_GPIO_PIN, gpio.DIR_OUT, gpio.EDGE_NONE, pinSet(BLUE_GPIO_PIN));
+gpio.setup(RED_GPIO_PIN, gpio.DIR_HIGH, pinSet(RED_GPIO_PIN));
+gpio.setup(GREEN_GPIO_PIN, gpio.DIR_HIGH, pinSet(GREEN_GPIO_PIN));
+gpio.setup(BLUE_GPIO_PIN, gpio.DIR_HIGH, pinSet(BLUE_GPIO_PIN));
 
 
 function pinSet(pinID) {
