3145c4264eb34d06e024b5f17296a9c863ad4c86
Cry <alexanderkatsero@gmail.com>
2017-09-12 00:48:55 +0300

    click event -> change event

--- public/script.js
+++ public/script.js
@@ -3,15 +3,15 @@
   const green = document.getElementById('green')
   const blue = document.getElementById('blue')
 
-  red.addEventListener('click', function() {
+  red.addEventListener('change', function() {
     if(red.checked) fetch('/red/1')
     else fetch('/red/0')
   })
-  green.addEventListener('click', function() {
+  green.addEventListener('change', function() {
     if(green.checked) fetch('/green/1')
     else fetch('/green/0')
   })
-  blue.addEventListener('click', function() {
+  blue.addEventListener('change', function() {
     if(blue.checked) fetch('/blue/1')
     else fetch('/blue/0')
   })
