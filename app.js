582fdeea429929030f8572e92cd2b0758fcf45aa
Cry <alexanderkatsero@gmail.com>
2017-09-11 23:42:11 +0300

    let there be light

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ const BLUE_GPIO_PIN = 15
 gpio.setup(RED_GPIO_PIN, gpio.DIR_HIGH)
 gpio.setup(GREEN_GPIO_PIN, gpio.DIR_HIGH)
 gpio.setup(BLUE_GPIO_PIN, gpio.DIR_HIGH)
+
+gpio.write(RED_GPIO_PIN, false, (err) => {
+  if (err) throw err
+})
+gpio.write(GREEN_GPIO_PIN, false, (err) => {
+  if (err) throw err
+})
+gpio.write(BLUE_GPIO_PIN, false, (err) => {
+  if (err) throw err
+})
