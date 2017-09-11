7f2facad5eaf1977befd5812755580d81867cbba
Cry <alexanderkatsero@gmail.com>
2017-09-11 23:35:16 +0300

    remove unnecessary logs

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ var RED_GPIO_PIN = 11
 var GREEN_GPIO_PIN = 13
 var BLUE_GPIO_PIN = 15
 
-gpio.setup(RED_GPIO_PIN, gpio.DIR_HIGH, pinSet(RED_GPIO_PIN));
-gpio.setup(GREEN_GPIO_PIN, gpio.DIR_HIGH, pinSet(GREEN_GPIO_PIN));
-gpio.setup(BLUE_GPIO_PIN, gpio.DIR_HIGH, pinSet(BLUE_GPIO_PIN));
-
-
-function pinSet(pinID) {
-    console.log('Setup to pin' + pinID);
-}
+gpio.setup(RED_GPIO_PIN, gpio.DIR_HIGH)
+gpio.setup(GREEN_GPIO_PIN, gpio.DIR_HIGH)
+gpio.setup(BLUE_GPIO_PIN, gpio.DIR_HIGH)
