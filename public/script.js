703b44aa3f4741914f0a494a7dbef528178f940a
Cry <alexanderkatsero@gmail.com>
2017-09-12 00:56:41 +0300

    fixed IIFE, removed logs, changed script loading order

--- public/script.js
+++ public/script.js
@@ -1,24 +1,19 @@
 (function() {
-  console.log('script.js init')
-
   const red = document.getElementById('red')
   const green = document.getElementById('green')
   const blue = document.getElementById('blue')
 
   red.addEventListener('change', function() {
-    console.log('red changed')
     if(red.checked) fetch('/red/1')
     else fetch('/red/0')
   })
   green.addEventListener('change', function() {
-    console.log('green changed')
     if(green.checked) fetch('/green/1')
     else fetch('/green/0')
   })
   blue.addEventListener('change', function() {
-    console.log('blue changed')
     if(blue.checked) fetch('/blue/1')
     else fetch('/blue/0')
   })
 
-})
+}())
