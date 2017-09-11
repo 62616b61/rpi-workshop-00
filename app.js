c0c04a8a6cef932891b0a026bfffbd35813295fd
Cry <alexanderkatsero@gmail.com>
2017-09-12 00:02:20 +0300

    red led control api

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const express = require('express')
 const gpio = require('rpi-gpio')
 
 const RED_GPIO_PIN = 11
@@ -8,8 +9,25 @@ gpio.setup(RED_GPIO_PIN, gpio.DIR_HIGH)
 gpio.setup(GREEN_GPIO_PIN, gpio.DIR_HIGH)
 gpio.setup(BLUE_GPIO_PIN, gpio.DIR_HIGH)
 
-setTimeout(() => {
-  gpio.write(RED_GPIO_PIN, false)
-  gpio.write(GREEN_GPIO_PIN, false)
-  gpio.write(BLUE_GPIO_PIN, false)
-}, 1000)
+const app = express()
+
+app.get('/red/:value', (request, response) => {
+  const value = request.params.value
+
+  if (value === "1") {
+    gpio.write(RED_GPIO_PIN, true)
+    res.send('ok')
+  }
+  else {
+    gpio.write(RED_GPIO_PIN, false)
+    res.send('ok')
+  }
+})
+
+// Listen on port 3000
+const server = app.listen(3000, () => {
+  const host = server.address().address
+  const port = server.address().port
+
+  console.log('Listening at http://%s:%s', host, port)
+})
