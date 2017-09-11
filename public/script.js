f9e84d02c6aa41254f8739b42145afcc64973252
Cry <alexanderkatsero@gmail.com>
2017-09-12 00:43:41 +0300

    NO JQUERY

--- public/script.js
+++ public/script.js
@@ -1,27 +1,19 @@
-$(function() {
+(function() {
+  const red = document.getElementById('red')
+  const green = document.getElementById('green')
+  const blue = document.getElementById('blue')
 
-    $('#red').click(function() {
-      if($('#red').prop('checked')) {
-          $.get( "/red/1" , function() {});
-      } else {
-          $.get( "/red/0" , function() {});
-        }
-    });
+  red.addEventListener('click', function() {
+    if(red.checked) fetch('/red/1')
+    else fetch('/red/0')
+  })
+  green.addEventListener('click', function() {
+    if(green.checked) fetch('/green/1')
+    else fetch('/green/0')
+  })
+  blue.addEventListener('click', function() {
+    if(blue.checked) fetch('/blue/1')
+    else fetch('/blue/0')
+  })
 
-    $('#green').click(function() {
-      if($('#green').prop('checked')) {
-          $.get( "/green/1" , function() {});
-      } else {
-          $.get( "/green/0" , function() {});
-        }
-    });
-
-    $('#blue').click(function() {
-      if($('#blue').prop('checked')) {
-          $.get( "/blue/1" , function() {});
-      } else {
-          $.get( "/blue/0" , function() {});
-        }
-    });
-
-});
+})
