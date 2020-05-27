2feb2b2123a6d36de770349f8a384c3e802aa912
Alex Katsero <alexanderkatsero@gmail.com>
2020-05-27 17:48:36 +0300

    Gamify it

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const gpio = require('rpi-gpio')
 
-const RED_GPIO_PIN = 11
-const GREEN_GPIO_PIN = 13
-const BLUE_GPIO_PIN = 15
+/*  Oh, no!!!
+ *  Look! Someone erased the pin numbers!
+ *
+ *  Without pin numbers the Raspberry Pi won't know how to control the LED!
+ *
+ *  Fortunately, you can fix this!
+ *  Find out the numbers of the pins that wires are connected to
+ *  and fill in the blanks down below!
+ */
 
-gpio.setup(RED_GPIO_PIN, gpio.DIR_HIGH)
-gpio.setup(GREEN_GPIO_PIN, gpio.DIR_HIGH)
-gpio.setup(BLUE_GPIO_PIN, gpio.DIR_HIGH)
+const RED_GPIO_PIN =
+const GREEN_GPIO_PIN =
+const BLUE_GPIO_PIN =
+
+gpio.setup(RED_GPIO_PIN, gpio.DIR_LOW)
+gpio.setup(GREEN_GPIO_PIN, gpio.DIR_LOW)
+gpio.setup(BLUE_GPIO_PIN, gpio.DIR_LOW)
 
 setTimeout(() => {
-  gpio.write(RED_GPIO_PIN, false)
-  gpio.write(GREEN_GPIO_PIN, false)
-  gpio.write(BLUE_GPIO_PIN, false)
+  gpio.write(RED_GPIO_PIN, true)
+  gpio.write(GREEN_GPIO_PIN, true)
+  gpio.write(BLUE_GPIO_PIN, true)
 }, 1000)
